c5b8f796841231046a0f64f9d391b9df5682251a
Om Sharma <113010856+omsharma-004@users.noreply.github.com>
2025-03-11 15:53:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -4,19 +4,18 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-  int helper(int n, vector<int>& dp){
-      if(n<=1) return 1;
-      if(dp[n]!=-1) return dp[n];
-      dp[n]=helper(n-1,dp)+helper(n-2,dp);
-      return dp[n];
-  }
-    // Function to count number of ways to reach the nth stair.
+  int helper(vector<int>& dp, int n){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=helper(dp,n-1)+helper(dp,n-2);
+        return dp[n];
+    }
     int countWays(int n) {
-        // your code here
         vector<int> dp(n+1,-1);
-        return helper(n, dp);
+        return helper(dp,n);
     }
 };
 
